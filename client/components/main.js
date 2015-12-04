ffcb1927f8d00b29bf75f737b289ce54d7993c8d
Jackie Wung <jacquelinewung@gmail.com>
2015-12-03 16:27:31 -0800

    [feat] main page checks token condition

--- client/components/main.js
+++ client/components/main.js
@@ -4,10 +4,18 @@ var Homepage = require('./homepage/homepage.js')
 
 var Main = React.createClass({
 
+  getInitialState: function() {
+    return {
+      hasToken: false
+    }
+  },
+
   render: function() {
+    //the main page is used to check which view to display
+    //depending on whether or not there is a token in local storage
     return (
       <div>
-        <Homepage />
+        {this.state.hasToken} ? <UserPage /> : <Homepage />
       </div>
     )
   }
