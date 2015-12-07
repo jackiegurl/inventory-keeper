c52bf0c53fe9a5c2179cc8ef51bf56abb92e839c
Jackie Wung <jacquelinewung@gmail.com>
2015-12-06 19:01:07 -0800

    [feat] add draft of closet header

--- client/components/userpage/closet/closet-header.js
+++ client/components/userpage/closet/closet-header.js
@@ -1,16 +1,30 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var Product = React.createClass({
+var ClosetHeader = React.createClass({
+
+  getInitialState: function() {
+    return {
+      viewStyle: "image"
+    }
+  },
+
+  toggleView: function() {
+    this.setState({ viewStyle: !this.state.viewStyle });
+  },
 
   render: function() {
     return (
-      <div>
-        <span>Table View</span>
-        <span>Image View</span>
+      <div className="closet-header-container">
+        <span className="closet-header-links" onClick={this.toggleView}>
+          Table View
+        </span>
+        <span className="closet-header-links" onClick={this.toggleView}>
+          Image View
+        </span>
       </div>
     )
   }
 });
 
-module.exports = Product;
+module.exports = ClosetHeader;
