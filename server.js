ee34d41c4918d2970de697d8a90fd58ead60a2d4
Jackie Wung <jacquelinewung@gmail.com>
2015-12-07 20:30:07 -0800

    package

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
+app.use('/', express.static(__dirname + "/client"));
 
 app.listen(process.env.PORT || 3000, function() {
   console.log("Listening to 3000...");
