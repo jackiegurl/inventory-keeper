7ea73fad99f9c9d13ca7df1277a94dfa0d045c72
Jackie Wung <jacquelinewung@gmail.com>
2015-12-07 20:14:04 -0800

    move bundle

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var app = express();
 var bodyParser = require("body-parser");
 
 app.use(bodyParser.json());
-app.use("/", express.static(__dirname + './client'));
-app.use('/assets', express.static(__dirname + '/../assets'));
+app.use("/", express.static(__dirname));
+app.use('/assets', express.static(__dirname + '../assets'));
 
 app.listen(process.env.PORT || 3000, function() {
   console.log("Listening to 3000...");
